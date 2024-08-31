ab4542df153deba663861323eb4d1c291380681b
rafa <rafaelcostatricolor@gmail.com>
2024-08-31 12:08:38 -0300

    adicionado o efeito Scroll Reveal

---
+++ reveal.js
@@ -0,0 +1,22 @@
+window.reveal = ScrollReveal({reset:true});
+
+// Scroll Reveal da primeira seção
+reveal.reveal('.efeito-txt-top',{
+    duration: 2000,
+    distance: '60px'
+})
+
+reveal.reveal('.efeito-img-top',{
+    duration: 2000,
+    distance: '60px'
+})
+
+reveal.reveal('.efeito-card-metodo',{
+    duration: 2000,
+    distance: '60px'
+})
+
+reveal.reveal('.efeito-generic',{
+    duration: 2000,
+    distance: '60px'
+})
